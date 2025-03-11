f0e4dd2aadbf31c2e06b41b31344a0aa40336ccb
Matteo <magdanet@proton.me>
2025-03-11 18:46:01 +0100

    fix: updaet

--- src/app/api/checkout/stripe/webhook/route.ts
+++ src/app/api/checkout/stripe/webhook/route.ts
@@ -12,8 +12,6 @@ export async function POST(req: Request) {
   let receivedEvent;
 
   try {
-    
-
     receivedEvent = await stripe.webhooks.constructEventAsync(
       payload,
       signature!,
@@ -31,6 +29,76 @@ export async function POST(req: Request) {
     //return res.status(200).send('');
   }
  
+  let order_id: number;
+  switch (receivedEvent.type) {
+    case 'payment_intent.canceled':
+    case 'payment_intent.payment_failed':
+      const payment = receivedEvent.data.object as Stripe.PaymentIntent;
+      order_id = Number(payment.metadata?.order_id);
+
+      console.warn(`Pagamento fallito per ${order_id}`);
+
+      if (payment.id && !isNaN(order_id)) {
+        /*await updateOrder(order_id, {
+          payment_id: payment.id,
+          payment_status: 'failed',
+          payment_date: dt.unix(payment.created).utc().format(),
+        });*/
+
+        /*await deleteEntries(order_id, {
+          payment_id: payment.id,
+          payment_status: 'failed',
+          payment_date: dt.unix(payment.created).utc().format(),
+        });*/
+      }
+      //await sendPaymentErrorMail();
+      break;
+    case 'checkout.session.expired':
+      const sessionExp = receivedEvent.data.object as Stripe.Checkout.Session;
+      order_id = Number(sessionExp.metadata?.order_id);
+
+      console.info(`Checkout expired per ${order_id}`);
+
+      if (sessionExp.payment_intent && !isNaN(order_id)) {
+        /*await updateOrder(order_id, {
+          payment_id: sessionExp.payment_intent as string,
+          payment_status: 'failed',
+          payment_date: dt.unix(sessionExp.created).utc().format(),
+        });*/
+      }
+      break;
+    case 'checkout.session.completed':
+      const session = receivedEvent.data.object as Stripe.Checkout.Session;
+      order_id = Number(session.metadata?.order_id);
+
+      console.info(`Checkout completed per ${order_id}`);
+      //const items = JSON.parse(base64.encode(session.metadata?.items));
+      //const order = JSON.parse(base64.encode(session.metadata?.q)) as Order;
+
+      /*if (session.payment_intent && !isNaN(order_id)) {
+        const order = await getOrder(order_id);
+
+        if (order === null) {
+          console.error(`Checkout completed in errore durante il recupero dell'ordine: ${JSON.stringify(session)}`);
+          return res.status(500).send('');
+        }
+
+        // verifica che session.payment_status === 'paid' ? 'paid' : 'awaiting',
+        /*await updateOrder(order_id, {
+          status: 'confirmed',
+          payment_id: session.payment_intent as string,
+          payment_status: 'paid',
+          payment_date: dt.unix(session.created).utc().format(),
+        });
+        //await sendConfirmationMail(order);
+      } else {
+        console.error(`Checkout completed terminato in errore: ${JSON.stringify(session)}`);
+      }*/
+      break;
+    default:
+      console.debug(`Arrivato evento non gestito: ${receivedEvent.type}`);
+      break;
+  }
   return new Response('Success!', {
     status: 200,
   })
