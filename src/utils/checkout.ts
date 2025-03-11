0186d65479736cfe43e1a94c64093f9d3a9bf264
Matteo <magdanet@proton.me>
2025-03-11 21:06:42 +0100

    feat: update

--- src/utils/checkout.ts
+++ src/utils/checkout.ts
@@ -5,19 +5,35 @@ import { createClient } from './supabase/server';
 import { FunctionsHttpError } from '@supabase/supabase-js';
 
 export async function createCheckout(orderData: any) {//Checkout) {
-  const supabase = await createClient()
+  /*const supabase = await createClient()
 
   const { data, error } = await supabase.functions.invoke('checkout', {
     method: 'POST',
     body: orderData
-  })
+  })*/
 
-  if (error instanceof FunctionsHttpError) {
+  const response = await fetch('/api/checkout', {
+    method: 'POST',
+    body: JSON.stringify(orderData),
+    headers: {
+      'content-type': 'application/json',
+      'cancel_url': '',
+    },
+    cache: 'no-cache',
+  });
+  const data = await response.json();
+
+  if (response.status !== 200) {
+    console.log(data)
+    throw new Error(data.error);
+  }
+
+  /*if (error instanceof FunctionsHttpError) {
     const { message } = await error.context.json()
     throw new Error(message)
   } else if (error) {
     throw new Error(error.message)
-  }
+  }*/
 
   redirect(data.checkoutSessionUrl)
 }
