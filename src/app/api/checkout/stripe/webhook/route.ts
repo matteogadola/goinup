081c5f0888d60d3623e192fbd67f05406632055a
Matteo <magdanet@proton.me>
2025-03-13 20:50:10 +0100

    fix: mail

--- src/app/api/checkout/stripe/webhook/route.ts
+++ src/app/api/checkout/stripe/webhook/route.ts
@@ -96,7 +96,13 @@ export async function POST(req: Request) {
           payment_status: 'paid',
           payment_date: dt.unix(session.created).utc().format(),
         });
-        await sendConfirmationMail(order);
+
+        const supabase = createClient()
+        const { data, error } = await supabase.functions.invoke('mail-legacy', {
+          method: 'POST',
+          body: order
+        })
+        //await sendConfirmationMail(order);
       } else {
         console.error(`Checkout completed terminato in errore: ${JSON.stringify(session)}`);
       }
