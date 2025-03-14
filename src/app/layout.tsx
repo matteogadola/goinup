bbcf37ccc6aafbc0cfde32b7a3143a4154cf65d3
Matteo <magdanet@proton.me>
2025-03-14 14:14:06 +0100

    feat: navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,26 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import Navbar from '@components/navbar'
-import Footer from '@components/footer'
 import { fonts } from '@utils/fonts'
-import { AppShell, Burger, MantineProvider } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks'
+
+import { AppShell, MantineProvider, AppShellHeader,
+  AppShellNavbar,
+  AppShellMain
+ } from '@mantine/core';
+import AppContainer from '@/components/ui/mantine/app';
+import Navbar from '@/components/ui/mantine/navbar';
+import Sidebar from '@/components/ui/mantine/sidebar';
+
+/*export const metadata: Metadata = {
+  title: "Goinup Vertical",
+  description: "Circuito di gare vertical a scopo benefico",
+};
+*/
+const nav = [
+  { name: "Home", path: "/", visible: false },
+  { name: "Classifiche", path: "/results" },
+  { name: "Foto e Video", path: "/media" },
+  { name: "Regolamento", path: "/regulation" },
+]
 
 export const metadata: Metadata = {
   title: "Goinup Vertical",
@@ -20,11 +36,19 @@ export default function RootLayout({
   return (
     <html lang="it">
       <body className={`${fonts.map(font => font.variable).join(' ')} antialiased flex flex-col min-h-screen`}>
-      <Navbar />
-        <div className="px-4 py-8">
-          {children}
-        </div>
-        <Footer />
+        <MantineProvider>
+          <AppContainer>
+            <AppShellHeader>
+              <Navbar nav={nav} />
+            </AppShellHeader>
+            <AppShellNavbar p="md">
+              <Sidebar nav={nav} />
+            </AppShellNavbar>
+            <AppShellMain>
+              {children}
+            </AppShellMain>
+          </AppContainer>
+        </MantineProvider>
       </body>
     </html>
   );
